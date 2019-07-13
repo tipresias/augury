1b823d087f9ea4997be8e477b76efc32e49b12b6
Craig Franklin <craigjfranklin@gmail.com>
2019-07-13 17:03:01 +1000

    Fix credential volume for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,11 @@
 
 set -euo pipefail
 
-docker-compose run -e PROJECT_ID=${PROJECT_ID} -e AFL_DATA_SERVICE=${AFL_DATA_SERVICE} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} -e GCPF_TOKEN=${GCPF_TOKEN} data_science sls deploy
+docker run \
+  -e PROJECT_ID=${PROJECT_ID} \
+  -e AFL_DATA_SERVICE=${AFL_DATA_SERVICE} \
+  -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} \
+  -e GCPF_TOKEN=${GCPF_TOKEN} \
+  -v ${HOME}/.gcloud:/app/.gcloud \
+  cfranklin11/tipresias_data_science:latest \
+  sls deploy
