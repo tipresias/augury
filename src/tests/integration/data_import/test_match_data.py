4fad0119aa5cfc3953f0bd321ef75e70b122c15e
Craig Franklin <craigjfranklin@gmail.com>
2019-07-28 16:09:43 +1000

    Add match data import function

---
+++ src/tests/integration/data_import/test_match_data.py
@@ -0,0 +1,82 @@
+import os
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from datetime import date, timedelta
+
+from betamax import Betamax
+from requests import Session
+
+from machine_learning.data_import.match_data import fetch_match_data
+from machine_learning.settings import CASSETTE_LIBRARY_DIR
+
+SEPT = 9
+MAR = 3
+FIFTEENTH = 15
+THIRTY_FIRST = 31
+AFL_DATA_SERVICE = os.getenv("AFL_DATA_SERVICE", default="")
+GCR_TOKEN = os.getenv("GCR_TOKEN", default="")
+ENV_VARS = os.environ.copy()
+DATA_IMPORT_PATH = "machine_learning.data_import"
+
+with Betamax.configure() as config:
+    config.cassette_library_dir = CASSETTE_LIBRARY_DIR
+    config.define_cassette_placeholder("<AFL_DATA_TOKEN>", GCR_TOKEN)
+    config.define_cassette_placeholder("<AFL_DATA_URL>", AFL_DATA_SERVICE)
+
+
+class TestMatchData(TestCase):
+    def setUp(self):
+        today = date.today()
+        # Season start and end are approximate, but defined to be safely after the
+        # usual start and before the usual end
+        end_of_previous_season = date(today.year - 1, SEPT, FIFTEENTH)
+        start_of_this_season = date(today.year, MAR, THIRTY_FIRST)
+        end_of_this_season = date(today.year, SEPT, FIFTEENTH)
+
+        a_year = timedelta(days=365)
+        a_year_ago = today - a_year
+
+        if today > start_of_this_season and a_year_ago < end_of_this_season:
+            self.start_date = str(a_year_ago)
+        elif today < start_of_this_season:
+            self.start_date = str(end_of_previous_season - a_year)
+        else:
+            self.start_date = str(end_of_this_season - a_year)
+
+        self.end_date = str(today)
+
+    def test_fetch_match_data(self):
+        data = fetch_match_data(
+            start_date=self.start_date, end_date=self.end_date, verbose=0
+        )
+
+        self.assertIsInstance(data, list)
+        self.assertIsInstance(data[0], dict)
+        self.assertTrue(any(data))
+
+        dates = {datum["date"] for datum in data}
+        self.assertLessEqual(self.start_date, min(dates))
+        self.assertGreaterEqual(self.end_date, max(dates))
+
+
+class TestMatchDataProd(TestCase):
+    def setUp(self):
+        self.session = Session()
+        self.start_date = "2012-01-01"
+        self.end_date = "2013-12-31"
+
+    @patch.dict(os.environ, {**ENV_VARS, **{"PYTHON_ENV": "production"}}, clear=True)
+    @patch(f"{DATA_IMPORT_PATH}.match_data.json.dump", MagicMock())
+    def test_fetch_match_data(self):
+        with Betamax(self.session).use_cassette("match_data"):
+            with patch(f"{DATA_IMPORT_PATH}.base_data.requests.get", self.session.get):
+                data = fetch_match_data(
+                    start_date=self.start_date, end_date=self.end_date, verbose=0
+                )
+                self.assertIsInstance(data, list)
+                self.assertIsInstance(data[0], dict)
+                self.assertTrue(any(data))
+
+                dates = {datum["date"] for datum in data}
+                self.assertLessEqual(self.start_date, min(dates))
+                self.assertGreaterEqual(self.end_date, max(dates))
---
+++ src/tests/unit/data_import/test_match_data.py
@@ -0,0 +1,39 @@
+import os
+from unittest import TestCase
+from unittest.mock import patch, mock_open
+import json
+
+from tests.fixtures.data_factories import fake_raw_match_results_data
+from machine_learning.settings import RAW_DATA_DIR
+from machine_learning.data_import.match_data import save_match_data
+
+
+START_DATE = "2012-01-01"
+START_YEAR = int(START_DATE[:4])
+END_DATE = "2013-12-31"
+END_YEAR = int(END_DATE[:4]) + 1
+N_MATCHES_PER_YEAR = 2
+MATCH_DATA_MODULE_PATH = "machine_learning.data_import.match_data"
+MATCH_DATA_PATH = os.path.join(RAW_DATA_DIR, f"match-data_{START_DATE}_{END_DATE}.json")
+
+
+class TestMatchData(TestCase):
+    def setUp(self):
+        self.fake_match_data = fake_raw_match_results_data(
+            N_MATCHES_PER_YEAR, (START_YEAR, END_YEAR)
+        ).to_dict("records")
+
+    @patch(f"{MATCH_DATA_MODULE_PATH}.fetch_match_data")
+    @patch("builtins.open", mock_open())
+    @patch("json.dump")
+    def test_save_match_data(self, _mock_json_dump, mock_fetch_data):
+        mock_fetch_data.return_value = self.fake_match_data
+
+        save_match_data(start_date=START_DATE, end_date=END_DATE, verbose=0)
+
+        mock_fetch_data.assert_called_with(
+            start_date=START_DATE, end_date=END_DATE, verbose=0
+        )
+        open.assert_called_with(MATCH_DATA_PATH, "w")
+        dump_args, _dump_kwargs = json.dump.call_args
+        self.assertIn(self.fake_match_data, dump_args)
