f0adbb352b5ddbedd7070df38b4025ae4332dc1c
Craig Franklin <craigjfranklin@gmail.com>
2019-09-03 08:38:45 +1000

    Remove unused TeamDataStacker & related code

--- src/machine_learning/data_processors/__init__.py
+++ src/machine_learning/data_processors/__init__.py
@@ -1,5 +1,4 @@
 """Module for data transformers"""
 
-from .team_data_stacker import TeamDataStacker
 from .feature_builder import FeatureBuilder
 from .feature_calculation import feature_calculator
