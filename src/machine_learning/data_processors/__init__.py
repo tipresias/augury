639c5f17992a1989c9a2016a6f0c4166663bb522
Craig Franklin <craigjfranklin@gmail.com>
2019-09-03 08:37:32 +1000

    Remove unused PlayerDataStacker & related code

--- src/machine_learning/data_processors/__init__.py
+++ src/machine_learning/data_processors/__init__.py
@@ -2,5 +2,4 @@
 
 from .team_data_stacker import TeamDataStacker
 from .feature_builder import FeatureBuilder
-from .player_data_stacker import PlayerDataStacker
 from .feature_calculation import feature_calculator
