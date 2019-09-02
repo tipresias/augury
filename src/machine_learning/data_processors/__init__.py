031d2514ef426315a7f5b045a972a2ae15e83cc3
Craig Franklin <craigjfranklin@gmail.com>
2019-09-03 08:36:18 +1000

    Remove unused PlayerDataAggregator & related code

--- src/machine_learning/data_processors/__init__.py
+++ src/machine_learning/data_processors/__init__.py
@@ -3,5 +3,4 @@
 from .team_data_stacker import TeamDataStacker
 from .feature_builder import FeatureBuilder
 from .player_data_stacker import PlayerDataStacker
-from .player_data_aggregator import PlayerDataAggregator
 from .feature_calculation import feature_calculator
