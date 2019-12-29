0f501dd848fbfbef8d534635337e208851bda764
Craig Franklin <craigjfranklin@gmail.com>
2019-12-30 09:59:28 +1100

    Add pickle GC data set to handle pickled models in buckets

---
+++ src/augury/io/pickle_gc_storage_data_set.py
@@ -0,0 +1,67 @@
+"""
+``JSONGCStorageDataSet`` loads and saves data to a file in Google Cloud Storage.
+Current assumption is that this only runs in the context of a Google Cloud Function,
+meaning that credentials are unnecessary (use local data files when running in
+dev environment).
+"""
+
+from typing import Any, Dict, List
+import os
+
+import pandas as pd
+import joblib
+from kedro.io.core import AbstractDataSet
+from google.cloud import storage
+
+from augury.settings import BASE_DIR
+
+
+class PickleGCStorageDataSet(AbstractDataSet):
+    """
+    ``PickleGCStorageDataSet`` loads and saves pickled objects to a file
+    in Google Cloud Storage.
+    """
+
+    def __init__(
+        self, filepath: str, bucket_name: str, project_dir: str = BASE_DIR
+    ) -> None:
+        """Creates a new instance of ``PickleGCStorageDataSet`` pointing to a concrete
+        pickle file on Google Cloud Storage.
+
+        Args:
+            filepath: Path to a pickle file.
+            bucket_name: GC Storage bucket name.
+            project_dir: Root directory for the project.
+        """
+
+        self._filepath = filepath
+        self._bucket_name = bucket_name
+        self._project_dir = project_dir
+
+    def _load(self) -> List[Dict[str, Any]]:
+        client = storage.Client()
+
+        bucket = client.get_bucket(self._bucket_name)
+        blob = bucket.get_blob(self._filepath)
+
+        local_file_dir = os.path.join(self._project_dir, "tmp")
+
+        if not os.path.exists(local_file_dir):
+            os.mkdir(local_file_dir)
+
+        local_filepath = os.path.join(local_file_dir, self._filepath)
+
+        with open(local_filepath, "wb") as file_obj:
+            blob.download_to_file(file_obj)
+
+        return joblib.load(local_filepath)
+
+    def _save(self, data: pd.DataFrame) -> None:
+        pass
+
+    def _describe(self) -> Dict[str, Any]:
+        return dict(
+            filepath=self._filepath,
+            bucket_name=self._bucket_name,
+            project_dir=self._project_dir,
+        )
