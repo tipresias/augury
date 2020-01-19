23e54a408c1ed4608e06675bc36e402478cda2eb
Craig Franklin <craigjfranklin@gmail.com>
2020-01-19 19:12:57 +1100

    Update model saving script & run it

--- scripts/save_default_models.py
+++ scripts/save_default_models.py
@@ -1,10 +1,13 @@
-"""
-Script for creating default models, training them, then pickling them.
+"""Script for creating default models, training them, then pickling them.
+
 Necessary due to how frequently changes to modules or package versions
 make old model files obsolete.
 """
 
+from joblib import Parallel, delayed
+import numpy as np
 
+from tests.fixtures.fake_estimator import pickle_fake_estimator
 from augury.ml_estimators import (
     BenchmarkEstimator,
     BaggingEstimator,
@@ -12,25 +15,34 @@ from augury.ml_estimators import (
     ConfidenceEstimator,
 )
 from augury.ml_data import MLData
-from tests.fixtures.fake_estimator import pickle_fake_estimator
+from augury.settings import SEED
 
 
-def train_save_model(model, data_set_name):
-    pipeline = "legacy" if "legacy" in data_set_name else "full"
-    data = MLData(data_set=data_set_name, pipeline=pipeline)
+np.random.seed(SEED)
+
+
+def _train_save_model(model, **data_kwargs):
+    data = MLData(**data_kwargs)
     model.fit(*data.train_data)
     model.dump()
 
 
 def main():
+    """Loop through models, training and saving each."""
+    legacy_data_kwargs = {"data_set": "legacy_model_data", "pipeline": "legacy"}
+    data_kwargs = {"data_set": "model_data", "pipeline": "full"}
+
     model_info = [
-        (BenchmarkEstimator(), "legacy_model_data"),
-        (BaggingEstimator(name="tipresias_2019"), "legacy_model_data"),
-        (StackingEstimator(), "model_data"),
-        (ConfidenceEstimator(), "model_data"),
+        (BenchmarkEstimator(), legacy_data_kwargs),
+        (BaggingEstimator(name="tipresias_2019"), legacy_data_kwargs),
+        (StackingEstimator(name="tipresias_2020"), data_kwargs),
+        (ConfidenceEstimator(), {**data_kwargs, "label_col": "result"}),
     ]
 
-    _ = [train_save_model(*model_and_data) for model_and_data in model_info]
+    Parallel(n_jobs=-1)(
+        delayed(_train_save_model)(model, **data_kwargs)
+        for model, data_kwargs in model_info
+    )
 
     pickle_fake_estimator()
 
