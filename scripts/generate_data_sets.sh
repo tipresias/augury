ea6d84d93e56337aca4fcdff745197ed41084e94
Craig Franklin <craigjfranklin@gmail.com>
2020-01-27 17:37:46 +1100

    Add pipeline data sets to script

--- scripts/generate_data_sets.sh
+++ scripts/generate_data_sets.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mkdir -p "${PWD}/data/01_raw"
+DATA_DIR="${PWD}/data"
+mkdir -p "${DATA_DIR}/01_raw" "${DATA_DIR}/02_intermediate" "${DATA_DIR}/05_model_input"
 
 DATA_IMPORT_DIR="${PWD}/src/augury/data_import"
 python3 "${DATA_IMPORT_DIR}/betting_data.py"
 python3 "${DATA_IMPORT_DIR}/match_data.py"
 python3 "${DATA_IMPORT_DIR}/player_data.py"
+
+kedro run --pipeline legacy -e development
+kedro run --pipeline full -e development
