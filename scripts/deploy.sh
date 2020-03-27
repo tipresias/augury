1c23ac41d073a2d7fd7f2f1f620c655324d01b1b
Craig Franklin <craigjfranklin@gmail.com>
2020-03-28 08:34:10 +1100

    Revert "Update CI config & scripts"
    This reverts commit 2fd496ab502cfe5e309b33c517ac801a599d5626.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,12 @@
 set -euo pipefail
 
 docker run \
+  -e PROJECT_ID=${PROJECT_ID} \
   -e AFL_DATA_SERVICE=${AFL_DATA_SERVICE} \
   -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} \
-  -e AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE} \
   -e GCPF_TOKEN=${GCPF_TOKEN} \
   -e GCR_TOKEN=${GCR_TOKEN} \
   -e PYTHON_ENV=production \
   -v ${HOME}/.gcloud:/app/.gcloud \
-  -v ${HOME}/.aws:/app/.aws \
   cfranklin11/tipresias_data_science:latest \
   npx sls deploy
