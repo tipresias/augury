6a18c826d2ee858e5c2463afa8b18451aa0070bc
Craig Franklin <craigjfranklin@gmail.com>
2020-06-04 20:34:07 +1000

    Add scripts for managing digital ocean droplet

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,46 @@
 
 set -euo pipefail
 
-# We need travis_wait for Travis CI builds, because installing R packages takes forever.
-# On a local machine, we can just build & deploy as normal.
-if [ -n "$(LC_ALL=C type -t travis_wait)" ] && [ "$(LC_ALL=C type -t travis_wait)" = function ]
+APP_DIR=/var/www/${PROJECT_ID}
+DOCKER_IMAGE=cfranklin11/tipresias_data_science:latest
+PORT=8008
+
+sudo chmod 600 ~/.ssh/deploy_rsa
+sudo chmod 755 ~/.ssh
+
+docker pull ${DOCKER_IMAGE}
+docker build --cache-from ${DOCKER_IMAGE} -t ${DOCKER_IMAGE} .
+docker push ${DOCKER_IMAGE}
+
+RUN_APP="
+  cd ${APP_DIR} \
+    && docker pull ${DOCKER_IMAGE} \
+    && docker stop ${PROJECT_ID}_app \
+    && docker container rm ${PROJECT_ID}_app \
+    && docker run \
+      -d \
+      - v .gcloud:/app/.gcloud \
+      --env-file .env \
+      -p ${PORT}:${PORT} \
+      -e PYTHON_ENV=production \
+      -e GOOGLE_APPLICATION_CREDENTIALS=.gcloud/keyfile.json \
+      -e PYTHONPATH=./src \
+      --name ${PROJECT_ID}_app \
+      ${DOCKER_IMAGE}
+"
+
+# We use 'ssh' instead of 'doctl compute ssh' to be able to bypass key checking.
+ssh -i ~/.ssh/deploy_rsa -oStrictHostKeyChecking=no \
+  ${DIGITAL_OCEAN_USER}@${PRODUCTION_HOST} \
+  ${RUN_APP}
+
+if [ $? != 0 ]
 then
-  travis_wait 30 gcloud builds submit --config cloudbuild.yaml
-else
-  gcloud builds submit --config cloudbuild.yaml
+  exit $?
 fi
 
-gcloud beta run deploy augury \
-  --image gcr.io/${PROJECT_ID}/augury \
-  --memory 2048Mi \
-  --region us-central1 \
-  --platform managed \
-  --update-env-vars AFL_DATA_SERVICE_TOKEN=${AFL_DATA_SERVICE_TOKEN},PYTHON_ENV=production,DATA_SCIENCE_SERVICE_TOKEN=${DATA_SCIENCE_SERVICE_TOKEN},AFL_DATA_SERVICE=${AFL_DATA_SERVICE},TIPRESIAS_APP_TOKEN=${TIPRESIAS_APP_TOKEN}
+./backend/scripts/wait-for-it.sh ${PRODUCTION_HOST}:${PORT} \
+  -t 60 \
+  -- ./scripts/post_deploy.sh
+
+exit $?
