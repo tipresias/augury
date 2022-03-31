e7dd4f57dc10cffd0bcd1d2492b6a4145f5ce841
Craig Franklin <craigjfranklin@gmail.com>
2022-04-01 08:35:38 +1100

    Update data scripts to have date params
    We use smaller data sets in production, so it's more convenient
    to be able to limit date ranges via the script rather than
    manually filtering afterwards.

--- scripts/generate_data_sets.sh
+++ scripts/generate_data_sets.sh
@@ -4,8 +4,7 @@ DATA_DIR="${PWD}/data"
 mkdir -p "${DATA_DIR}/01_raw" "${DATA_DIR}/02_intermediate" "${DATA_DIR}/05_model_input"
 
 DATA_IMPORT_DIR="${PWD}/src/augury/data_import"
-python3 "${DATA_IMPORT_DIR}/betting_data.py"
 python3 "${DATA_IMPORT_DIR}/match_data.py"
 python3 "${DATA_IMPORT_DIR}/player_data.py"
 
-kedro run --pipeline full
+kedro run --pipeline full $*
